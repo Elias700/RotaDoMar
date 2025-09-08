09d65a4f48e0658bd9b8840b930c16cf5b51d77c
Elias700 <elias_ribeiro07@outlook.com.br>
2025-09-08 11:50:11 -0300

    feat: organizing the entire folder structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component'; // Importe o HeaderComponent
-import { FooterComponent } from './components/footer/footer.component'; // Importe o FooterComponent
+import { HeaderComponent } from './components/header/header.component'; 
+import { FooterComponent } from './components/footer/footer.component'; 
+import { ContactComponent } from './pages/home/contact/contact.component';
+import { MapComponent } from './pages/map/map.component';
+import { WeatherComponent } from './pages/home/weather/weather.component';
+import { BeachesComponent } from './pages/beaches/beaches.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent], // Adicione-os aqui
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ContactComponent, MapComponent, WeatherComponent, BeachesComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/home/beaches/beaches.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beaches',
+  imports: [],
+  templateUrl: './beaches.component.html',
+  styleUrl: './beaches.component.css'
+})
+export class BeachesComponent {
+
+}
