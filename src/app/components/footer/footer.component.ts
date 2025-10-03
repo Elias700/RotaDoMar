985fd24a3c433c5d4dd4f540de92a35ef8940778
Elias700 <elias_ribeiro07@outlook.com.br>
2025-10-03 08:17:49 -0300

    feat: final adjustments.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PrivacyModalComponent } from '../../components/privacy-modal/privacy-modal.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, PrivacyModalComponent],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  isPrivacyModalOpen = false;
 
+  openPrivacyModal() {
+    this.isPrivacyModalOpen = true;
+  }
+
+  closePrivacyModal() {
+    this.isPrivacyModalOpen = false;
+  }
 }
